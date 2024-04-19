72219c5996e14741f16cef9d818f67d59029d9c1
ksharykinpjwstk <k.sharykin@pjwstk.edu.pl>
2024-04-19 13:20:01 +0200

    Create country repository

---
+++ Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Interfaces/ICountryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tutorial7.Showcase.Entities;
+
+namespace Tutorial7.Showcase.Infrastrucutre;
+
+public interface ICountryRepository
+{
+    public Task<IEnumerable<Country>> Get();
+    public Task<Country?> GetById(int id);
+}
---
+++ Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Repositories/CountryRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using Tutorial7.Showcase.Entities;
+
+namespace Tutorial7.Showcase.Infrastrucutre;
+
+public class CountryRepository(string connectionString) : ICountryRepository
+{
+    public async Task<IEnumerable<Country>> Get()
+    {
+        List<Country> countries = [];
+
+        const string selectSchools = "SELECT * FROM Country";
+        using var con = new SqlConnection(connectionString);
+        using var com = new SqlCommand(selectSchools);
+
+        SqlDataReader reader = await com.ExecuteReaderAsync();
+        if (reader.HasRows) 
+        {
+            while(reader.Read())
+            {
+                var country = new Country {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1)
+                };
+                countries.Add(country);
+            }
+        }
+        
+        return countries;
+    }
+
+    public async Task<Country?> GetById(int id)
+    {
+        Country? country = null;
+        
+        const string selectSchools = "SELECT Top 1 * FROM Country WHERE Id = @Id";
+        using var con = new SqlConnection(connectionString);
+        using var com = new SqlCommand(selectSchools);
+
+        com.Parameters.AddWithValue("@Id", id);
+
+        SqlDataReader reader = await com.ExecuteReaderAsync();
+        if (reader.HasRows) 
+        {
+            while(reader.Read())
+            {
+                country = new Country {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1)
+                };
+            }
+        }
+
+        return country;
+    }
+}
