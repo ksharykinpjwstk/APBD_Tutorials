4f7c199bc1a0974a68c8d5f5262a130f4bcc75a8
ksharykinpjwstk <k.sharykin@pjwstk.edu.pl>
2024-04-19 12:59:22 +0200

    Implement Add school in repo

--- Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Interfaces/ISchoolRepository.cs
+++ Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Interfaces/ISchoolRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Tutorial7.Showcase.Infrastrucutre;
+﻿using Tutorial7.Showcase.Entities;
+
+namespace Tutorial7.Showcase.Infrastrucutre;
 
 public interface ISchoolRepository
 {
-
+    Task<bool> Add(School newSchool);
+    Task<IEnumerable<School>> Get();
+    Task<School?> GetById(int id);
+    Task<bool> Update(School school);
+    Task<bool> Delete(School school);
 }
--- Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Repositories/SchoolRepository.cs
+++ Tutorial7.Showcase/src/Tutorial7.Showcase.Infrastrucutre/Repositories/SchoolRepository.cs
@@ -1,6 +1,63 @@
-﻿namespace Tutorial7.Showcase.Infrastrucutre;
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Tutorial7.Showcase.Entities;
 
-public class SchoolRepository
+namespace Tutorial7.Showcase.Infrastrucutre;
+
+public class SchoolRepository(string connectionString) : ISchoolRepository
 {
+    public async Task<bool> Add(School newSchool)
+    {
+        int result;
+        using var con = new SqlConnection(connectionString);
+        if (newSchool.Description is null) 
+        {
+            using var com = new SqlCommand("AddSchoolWithoutDescription", con) 
+            {
+                CommandType = CommandType.StoredProcedure
+                
+            };
 
+            com.Parameters.Add("@IdCity", SqlDbType.Int).Value = newSchool.CityId;
+            com.Parameters.Add("@SchoolName", SqlDbType.VarChar).Value = newSchool.Name;
+            com.Parameters.Add("@StudentCount", SqlDbType.Int).Value = newSchool.StudentCoutn;
+
+            await con.OpenAsync();
+            result = await com.ExecuteNonQueryAsync();
+        }
+        else 
+        {
+            var sqlTextCom = @"INSERT INTO School(CityId, SchoolName, StudentCount, Description) VALUES 
+                            (@CityId, @SchoolName, @StudentCount, @Description)";
+            using var com = new SqlCommand(sqlTextCom);
+            com.Parameters.AddWithValue("@CityId", newSchool.CityId);
+            com.Parameters.AddWithValue("@SchoolName", newSchool.Name);
+            com.Parameters.AddWithValue("@StudentCount", newSchool.StudentCoutn);
+            com.Parameters.AddWithValue("@Description", newSchool.Description);
+
+            result = await com.ExecuteNonQueryAsync();
+        }
+
+        return result > 0;
+    }
+
+    public Task<bool> Delete(School school)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<School>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<School?> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(School school)
+    {
+        throw new NotImplementedException();
+    }
 }
